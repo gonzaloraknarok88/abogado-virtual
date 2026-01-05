8a145501b655e5cf80470742ae3e64020c5a0c59
gonzaloraknarok88 <189512486+gonzaloraknarok88@users.noreply.github.com>
2026-01-05 06:10:50 -0300

    Refactor next.config.js for improved formatting
    Formatted the next.config.js file for better readability.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-/** @type {import('next').NextConfig} */ const nextConfig = { reactStrictMode: true, swcMinify: true, compiler: { removeConsole: false }, typescript: { tsconfigPath: './tsconfig.json' } }; module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: false,
+  },
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
+};
+
+module.exports = nextConfig;
